fbae712865fa6a9b604b062c5d3dd108209b0498
cavidanhasanli <cavidan.hasanli@mail.ru>
2021-02-04 18:37:10 +0400

    commit-model

--- upload_app/models.py
+++ upload_app/models.py
@@ -2,7 +2,9 @@ import os
 
 from django.db import models
 from django.contrib.auth import get_user_model
+
 User = get_user_model()
+
 
 # Create your models here.
 
@@ -13,7 +15,7 @@ def content_file_name(instance, filename):
 
 
 class CreateFiles(models.Model):
-    user_id = models.ForeignKey(User, on_delete=models.CASCADE, null=True,blank=True)
+    user_id = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True)
     file_name = models.CharField(max_length=50, null=True, blank=True)
     my_file = models.FileField(upload_to=content_file_name, null=True, blank=True)
     file_description = models.CharField(max_length=255, null=True, blank=True)
@@ -25,8 +27,16 @@ class CreateFiles(models.Model):
 
 class SenderFiles(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE, blank=True, null=True, related_name='user_id')
-    sended_users = models.ForeignKey(User,on_delete=models.CASCADE,null=True,blank=True)
-    sended_files = models.ForeignKey(CreateFiles,on_delete=models.CASCADE,null=True,blank=True)
+    sended_users = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True)
+    sended_files = models.ForeignKey(CreateFiles, on_delete=models.CASCADE, null=True, blank=True)
 
     def __str__(self):
         return f'{self.sended_users} / {self.sended_files}'
+
+
+class Comment(models.Model):
+    sender_file = models.ForeignKey(SenderFiles, on_delete=models.CASCADE, null=True, blank=True)
+    comment = models.TextField(null=True,blank=True)
+
+    def __str__(self):
+        return f'{self.sender_file} / {self.comment}'
