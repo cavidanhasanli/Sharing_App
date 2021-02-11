3362cf973802b660cc7479c3961cbc74f7c7144a
cavidanhasanli <cavidan.hasanli@mail.ru>
2021-02-11 14:16:19 +0400

    fixs upload

--- Sharing_Settings/urls.py
+++ Sharing_Settings/urls.py
@@ -17,7 +17,6 @@ from django.contrib import admin
 from django.urls import path,include
 from django.conf.urls.static import static
 from django.conf import settings
-from .settings.prod import *
 
 
 urlpatterns = [
@@ -26,5 +25,6 @@ urlpatterns = [
     path('home/',include('upload_app.urls'))
 ]
 
-urlpatterns += static(STATIC_URL, document_root=STATIC_ROOT)
-urlpatterns += static(MEDIA_URL, document_root=MEDIA_ROOT)
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
